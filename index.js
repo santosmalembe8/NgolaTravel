bba953e8688ed940d14afe21af7b003da97ca063
Santos Malembe <santosmalembe8@gmail.com>
2026-04-22 22:02:46 +0100

    Actualização
    Aletração do menu

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
 const btn = document.getElementById('btn_mobile');
 const nav = document.getElementById('nav_bar');
-const overlay = document.getElementById('overlay');
 
-/* Toggle menu */
+/* Abrir/fechar menu */
 btn.addEventListener('click', () => {
     nav.classList.toggle('mostrar');
     btn.classList.toggle('active');
-    overlay.classList.toggle('active');
 });
 
-/* Fechar menu ao clicar overlay */
-overlay.addEventListener('click', () => {
-    nav.classList.remove('mostrar');
-    btn.classList.remove('active');
-    overlay.classList.remove('active');
-});
-
-/* Apenas fecha menu (SEM bloquear link) */
+/* Fechar menu ao clicar num link (sem interferir na navegação) */
 document.querySelectorAll('#nav_bar a').forEach(link => {
     link.addEventListener('click', () => {
         nav.classList.remove('mostrar');
         btn.classList.remove('active');
-        overlay.classList.remove('active');
     });
 });
